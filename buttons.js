4fc87274799806a546a5320c4cbee2d293134e03
Jakub Chrpa <ChrpaJakub@sssvt.cz>
2021-05-17 19:07:30 +0200

    Changes

--- buttons.js
+++ buttons.js
@@ -1,6 +1,6 @@
 rcmail.addEventListener('init', function(evt) {
-    $(".formbuttons").append("<a id='undoButton' style='position:relative;top: 1px;padding:10px;border-radius: 4px;border:1px solid rgb(55,190,255);background-color: rgb(0,106,157);color: rgb(226,232,233);z-index: 9999' href='javascript:void(0)'>Undo last</a>");
-    $(".formbuttons").append("<a id='sendButton' style='position:relative;left: 11px;top: 1px;padding:10px;border-radius: 4px;border:1px solid rgb(55,190,255);background-color: rgb(0,106,157);color: rgb(226,232,233);z-index: 9999' href='javascript:void(0)'>Send last</a>");
+    $(".formbuttons").append("<a id='undoButton' style='position:relative;top: 1px;padding:10px;border-radius: 4px;border:1px solid rgb(55,190,255);background-color: rgb(0,106,157);color: rgb(226,232,233);z-index: 9999' href='javascript:void(0)'>Undo</a>");
+    $(".formbuttons").append("<a id='sendButton' style='position:relative;left: 11px;top: 1px;padding:10px;border-radius: 4px;border:1px solid rgb(55,190,255);background-color: rgb(0,106,157);color: rgb(226,232,233);z-index: 9999' href='javascript:void(0)'>Send now</a>");
     $("#undoButton").hide();
     $("#sendButton").hide();
     $("#messagestack").css("opacity","0");
@@ -11,6 +11,7 @@ rcmail.addEventListener('init', function(evt) {
         console.log(resp);
     })
     $("#rcmbtn112").click(function(){
+        enableSend = true;
         $("#undoButton").show();
         $("#sendButton").show();
         $(window).bind('beforeunload', function(){
@@ -30,17 +31,18 @@ rcmail.addEventListener('init', function(evt) {
                 if(enableSend == true) {
                     var objtext = rcmail.http_get('plugin.sendMail');
                     alert('Email sent');
+                    window.history.back();
                 }
                 enableSend = false;
-                window.history.back();
             }
         }, 1000);
     })
     $("#undoButton").click(function (){
         var obj = rcmail.http_get('plugin.cancelMail');
         enableSend = false;
-        window.history.back();
         console.log(obj);
+        $("#undoButton").hide();
+        $("#sendButton").hide();
     });
     $("#sendButton").click(function (){
         if(enableSend == true) {
