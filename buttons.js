8fc9d6fdec9153e0990499fd742a31c6a1326bb3
Jakub Chrpa <ChrpaJakub@sssvt.cz>
2021-05-17 14:19:23 +0200

    changes

--- buttons.js
+++ buttons.js
@@ -1,19 +1,23 @@
 rcmail.addEventListener('init', function(evt) {
-    $(".formbuttons").append("<a id='testButton' style='position:relative;top: 1px;padding:10px;border-radius: 4px;border:1px solid rgb(55,190,255);background-color: rgb(0,106,157);color: rgb(226,232,233);z-index: 9999' href='javascript:void(0)'>Undo last</a>");
-    $(".formbuttons").append("<a id='basedButton' style='position:relative;left: 11px;top: 1px;padding:10px;border-radius: 4px;border:1px solid rgb(55,190,255);background-color: rgb(0,106,157);color: rgb(226,232,233);z-index: 9999' href='javascript:void(0)'>Send last</a>");
-    $("#testButton").hide();
-    $("#basedButton").hide();
+    $(".formbuttons").append("<a id='undoButton' style='position:relative;top: 1px;padding:10px;border-radius: 4px;border:1px solid rgb(55,190,255);background-color: rgb(0,106,157);color: rgb(226,232,233);z-index: 9999' href='javascript:void(0)'>Undo last</a>");
+    $(".formbuttons").append("<a id='sendButton' style='position:relative;left: 11px;top: 1px;padding:10px;border-radius: 4px;border:1px solid rgb(55,190,255);background-color: rgb(0,106,157);color: rgb(226,232,233);z-index: 9999' href='javascript:void(0)'>Send last</a>");
+    $("#undoButton").hide();
+    $("#sendButton").hide();
     $("#messagestack").css("opacity","0");
+
+    var enableSend = true;
 
     rcmail.addEventListener('plugin.callback', function (resp){
         alert("Massage arrived sir");
         console.log(resp);
     })
     $("#rcmbtn112").click(function(){
-        $("#testButton").show();
-        $("#basedButton").show();
+        $("#undoButton").show();
+        $("#sendButton").show();
         $(window).bind('beforeunload', function(){
-            var objtext = rcmail.http_get('plugin.sendMail');
+            if(enableSend == true) {
+                var objtext = rcmail.http_get('plugin.sendMail');
+            }
         });
         var countDownDate = new Date().getTime()+8*1000;
 
@@ -24,19 +28,25 @@ rcmail.addEventListener('init', function(evt) {
 
             if (distance < 0) {
                 clearInterval(x);
-                var objtext = rcmail.http_get('plugin.sendMail');
+                if(enableSend == true) {
+                    var objtext = rcmail.http_get('plugin.sendMail');
+                }
                 alert('Email sent');
                 window.history.back();
             }
         }, 1000);
     })
-    $("#testButton").click(function (){
+    $("#undoButton").click(function (){
         var obj = rcmail.http_get('plugin.cancelMail');
+        enableSend = false;
         window.history.back();
         console.log(obj);
     });
-    $("#basedButton").click(function (){
-        var obj = rcmail.http_get('plugin.sendMail');
+    $("#sendButton").click(function (){
+        if(enableSend == true) {
+            var objtext = rcmail.http_get('plugin.sendMail');
+        }
+        enableSend = false;
         window.history.back();
         console.log(obj);
     });
