21627ff9ad4ff04d9d02dc062ed14924b933bda0
sssvt-chrpa-jakub <chrpajakub@sssvt.cz>
2021-06-22 21:09:21 +0200

    Changes.

--- buttons.js
+++ buttons.js
@@ -1,35 +1,42 @@
 rcmail.addEventListener('init', function(evt) {
     $(".formbuttons").append("<a id='undoButton' style='position:relative;top: 1px;padding:10px;border-radius: 4px;border:1px solid rgb(55,190,255);background-color: rgb(0,106,157);color: rgb(226,232,233);z-index: 9999' href='javascript:void(0)'>Undo</a>");
-    $(".formbuttons").append("<a id='sendButton' style='position:relative;left: 11px;top: 1px;padding:10px;border-radius: 4px;border:1px solid rgb(55,190,255);background-color: rgb(0,106,157);color: rgb(226,232,233);z-index: 9999' href='javascript:void(0)'>Send now</a>");
     $("#undoButton").hide();
     $("#sendButton").hide();
     $("#messagestack").css("opacity","0");
     var enableSend = true;
 
-    rcmail.addEventListener('plugin.callback', function (resp){
+    rcmail.addEventListener('plugin.callback', function (resp) {
         alert("Massage arrived sir");
         console.log(resp);
     })
-    $("#rcmbtn112").click(function(){
+    $("#rcmbtn112").click(function () {
         window.onbeforeunload = null;
         enableSend = true;
+        $("#rcmbtn112").text = "Send now";
         $("#undoButton").show();
         $("#sendButton").show();
-        $(window).bind('beforeunload', function(){
-            if(enableSend == true) {
+        $(window).bind('beforeunload', function () {
+            if (enableSend == true) {
                 var objtext = rcmail.http_get('plugin.sendMail');
             }
         });
-        var countDownDate = new Date().getTime()+8*1000;
+        if (enableSend == true) {
+            $("#rcmbtn112").click(function () {
+                var objtext = rcmail.http_get('plugin.sendMail');
+                enableSend = false;
+                window.history.back();
+                console.log(obj);
+            })
+        }
+        var countDownDate = new Date().getTime() + 8 * 1000;
 
-        var x = setInterval(function() {
+        var x = setInterval(function () {
             var now = new Date().getTime();
             var distance = countDownDate - now;
             var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
             if (distance < 0) {
                 clearInterval(x);
-                if(enableSend == true) {
+                if (enableSend == true) {
                     var objtext = rcmail.http_get('plugin.sendMail');
                     alert('Email sent');
                     window.history.back();
@@ -44,13 +51,5 @@ rcmail.addEventListener('init', function(evt) {
         console.log(obj);
         $("#undoButton").hide();
         $("#sendButton").hide();
-    });
-    $("#sendButton").click(function (){
-        if(enableSend == true) {
-            var objtext = rcmail.http_get('plugin.sendMail');
-        }
-        enableSend = false;
-        window.history.back();
-        console.log(obj);
     });
 });
--- undo_email.php
+++ undo_email.php
@@ -1,9 +1,9 @@
 <?php
+
 class undo_email extends rcube_plugin
 {
     public $task = 'mail';
     private $map;
-
 
 
     function init()
@@ -17,14 +17,15 @@ class undo_email extends rcube_plugin
 
     function deleteMail()
     {
-        $sql= ("DELETE FROM unsent_emails ORDER BY email_id DESC LIMIT 1");
+        $sql = ("DELETE FROM unsent_emails ORDER BY email_id DESC LIMIT 1");
         $this->rc->db->query($sql);
     }
 
-    function sendMail(){
+    function sendMail()
+    {
         $query = "select * from unsent_emails order by email_id desc limit 1";
 
-        foreach ($this->rc->db->query($query) as $row){
+        foreach ($this->rc->db->query($query) as $row) {
             $from = $row["sender_mail"];
             $to = $row["receiver_mail"];
             $mailBody = $row["mail_body"];
@@ -43,52 +44,52 @@ class undo_email extends rcube_plugin
         $mime->setHTMLBody($htmlBody);
         $mime->get(['head_charset' => 'utf-8']);
         $mime->get(['text_charset' => 'utf-8']);
-        $mime->headers(['BeforeSend' => 'false', 'From' => $from,'Subject' => $subject, 'Date' => $dateTimeNow]);
+        $mime->headers(['BeforeSend' => 'false', 'From' => $from, 'Subject' => $subject, 'Date' => $dateTimeNow]);
 
         $smpt_opts = ['dsn' => 'false'];
         $smtp_error = null;
         $mailbody_file = null;
 
-        $this->rc->deliver_message($mime,$from,$to,$smtp_error,$mailbody_file,$smpt_opts,true);
+        $this->rc->deliver_message($mime, $from, $to, $smtp_error, $mailbody_file, $smpt_opts, true);
     }
 
     function mbs($args)
     {
-        if($args['message']->headers()['BeforeSend'] != 'false') {
-        $txt = var_export($args['message'], true);
+        if ($args['message']->headers()['BeforeSend'] != 'false') {
+            $txt = var_export($args['message'], true);
 
-        $regex = "/'txtbody' => (.+),.*\n.*'htmlbody'.*=> (.*),[\S\s]*'Subject' => '(.*)'/m";
-        preg_match($regex, $txt, $re);
-        for ($i = 0; $i < count($re); $i++) {
-            $entry = $re[$i];
-            if (strtolower($entry) == 'null') {
-                $re[$i] = '';
-            } else if (strlen($entry) > 1 && $entry[0] == "'" && $entry[strlen($entry) - 1] == "'") {
-                $re[$i] = substr($entry, 1, strlen($entry) - 2);
+            $regex = "/'txtbody' => (.+),.*\n.*'htmlbody'.*=> (.*),[\S\s]*'Subject' => '(.*)'/m";
+            preg_match($regex, $txt, $re);
+            for ($i = 0; $i < count($re); $i++) {
+                $entry = $re[$i];
+                if (strtolower($entry) == 'null') {
+                    $re[$i] = '';
+                } else if (strlen($entry) > 1 && $entry[0] == "'" && $entry[strlen($entry) - 1] == "'") {
+                    $re[$i] = substr($entry, 1, strlen($entry) - 2);
+                }
             }
+
+            $mailBody = $re[1];
+            $htmlBody = $re[2];
+            $mailSubject = $re[3];
+            $to = $args['mailto'];
+            $from = $args['from'];
+
+            $args['abort'] = true;
+            $args['error'] = null;
+            $args['result'] = false;
+
+
+            try {
+                $sqlquery = "insert into unsent_emails (receiver_mail,sender_mail,mail_body,html_body,subject) values ('$to','$from','$mailBody','$htmlBody','$mailSubject')";
+                $this->rc->db->query($sqlquery);
+            } catch (Exception $e) {
+            }
+            return $args;
         }
-
-        $mailBody = $re[1];
-        $htmlBody = $re[2];
-        $mailSubject = $re[3];
-        $to = $args['mailto'];
-        $from = $args['from'];
-
-        $args['abort'] = true;
-        $args['error'] = null;
-        $args['result'] = false;
-
-
-        try {
-            $sqlquery = "insert into unsent_emails (receiver_mail,sender_mail,mail_body,html_body,subject) values ('$to','$from','$mailBody','$htmlBody','$mailSubject')";
-            $this->rc->db->query($sqlquery);
-        }
-        catch (Exception $e) {
-        }
-        return $args;
     }
-}
 
 }
+
 ?>
 
